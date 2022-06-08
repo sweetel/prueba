9ef5b0e9e5396832d33873b4b37eb4b43861307d
usuario <usuario@usuario-PC>
2022-06-08 09:28:39 +0200

    PRIMERA VERSION PRUEBA

--- src/proyecto1/Proyecto1.java
+++ src/proyecto1/Proyecto1.java
@@ -2,7 +2,7 @@ package proyecto1;
 public class Proyecto1 {
     public static void main(String[] args) {
         int i=1;
-        while(i<10)
+        while(i<12)
             System.out.println(i++);
         
     }
